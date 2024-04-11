f95577edcc09341d753bab415dc6da6ac3f0b4d6
EmersonFernandez <fernandezreginoe@gmail.com>
2024-04-11 08:56:42 -0500

    primer commit

--- src/controllers/Cliente-app/CarProducts.jsx
+++ src/controllers/Cliente-app/CarProducts.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './CarProducts.css'; // Importa estilos CSS para el componente
 
 function CarProducts({ productsData }) {
